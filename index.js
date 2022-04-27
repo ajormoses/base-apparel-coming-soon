8ffc49a6e332848f80dc1da03887f6b265fb12f3
ajormoses <ajormoses@gmail.com>
2022-04-27 17:08:36 +0100

    completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector(".btn");
 const email = document.querySelector("#email");
 var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+const errorText = document.querySelector(".error-text");
+const errorIcon = document.querySelector(".error-icon");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -20,14 +22,14 @@ btn.addEventListener("click", (e) => {
 
 function error(element, msg) {
   element.style.border = "2px solid hsl(0, 93%, 68%)";
-  const parent = element.parentElement;
-  const p = parent.querySelector("p");
-  const error = parent.querySelector(".error-icon");
-  error.style.display = "block";
-  p.innerHTML = msg;
-  p.style.color = "hsl(0, 93%, 68%)";
+  errorText.textContent = msg;
+  errorText.style.display = "block";
+  errorText.style.color = "hsl(0, 93%, 68%)";
+  errorIcon.style.display = "block";
 }
 
 function success(element) {
-  element.style.border = "2px solid transparent)";
+  element.style.border = "2px solid hsl(0, 80%, 86%)";
+  errorText.style.display = "none";
+  errorIcon.style.display = "none";
 }
